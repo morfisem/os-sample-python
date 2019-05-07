9e4a988f2d87ce1574e5dd00f36b1fb934c3884b
morfisem <50360954+morfisem@users.noreply.github.com>
2019-05-07 12:40:46 +0300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Getting ready!!!!!!"
 
 if __name__ == "__main__":
     application.run()
